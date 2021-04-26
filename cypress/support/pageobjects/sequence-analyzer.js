b0c3d85d6966edb63007980d909bc3dea0462caa
vmelnyk <melyav92@gmail.com>
2021-04-26 22:47:24 +0300

    - Added sequence analyzer file to approve sequences automatically

---
+++ cypress/support/pageobjects/sequence-analyzer.js
@@ -0,0 +1,32 @@
+Cypress.Commands.add("Login", function (username, password) {
+    cy.get('#Username').type(username)
+    cy.get('#Password').type(password)
+    cy.get('#login-button').click()
+})
+
+
+export class SequenceAnalyzer {
+    gotItButton (){
+     return cy.get('button[data-action-type="OK"]')
+    };
+
+    frameEditor(){
+        return cy.get('#markers-canvas')
+    };
+
+    selectItemInContextMenu(){
+      return cy.get('.scp-sa-ctx-menu-list-item')
+    }
+
+   approveObservationBoundingBox(){
+        return cy.get('.scp-sa-edit-mode-apply.scp-sa-edit-mode-btn')
+   }
+   approveAllButton(){
+        return cy.get('#scp-sa-complete-all-contexts')
+    }
+
+    confirmApprove(){
+    return cy.get('.scp-sa-popup-btn.scp-sa-action-btn.scp-sa-element-to-focus')
+    }
+
+}
