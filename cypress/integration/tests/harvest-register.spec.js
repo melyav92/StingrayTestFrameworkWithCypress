f77ce69ee9e650d0d924c10b330de93a70caf797
vmelnyk <melyav92@gmail.com>
2021-06-27 23:40:24 +0300

    added test which should verify added data in the report added test which should update and verify the report with new values added test which should delete the report (should be updated)

--- cypress/integration/tests/harvest-register.spec.js
+++ cypress/integration/tests/harvest-register.spec.js
@@ -17,7 +17,14 @@ const fishPenAfterHarvestValueForM2 = 64000;
 const penM2Comment = "Comment for pen M2";
 const successfulToasterPopupMessage = 'Saved successfully';
 const reportDate = Cypress.moment().format("DD/MM/YYYY");
-
+const updatedFishHarvestedValueForPenM1 = 70000;
+const updatedAverageHarvestWeightValueForM1 = 7500;
+const updatedFishPenAfterHarvestValueForM1 = 50000;
+const updatedPenM1Comment = "Updated Comment for pen M1";
+const updatedFishHarvestedValueForPenM2 = 80000;
+const updatedAverageHarvestWeightValueForM2 = 4500;
+const updatedFishPenAfterHarvestValueForM2 = 10000;
+const updatedPenM2Comment = "Updated Comment for pen M2";
 
 let harvestRegister = new HarvestRegister();
 let login = new LoginPage()
@@ -27,6 +34,7 @@ let login = new LoginPage()
 describe('Harvest register',function (){
     beforeEach(function (){
        cy.visit('/en/Authentication/Login/?ReturnUrl=%2fen%2fHarvest%2fRegister')
+       //cy.visit('https://192.168.10.49:8100/en/Harvest/Register')
        login.loginToThePage(username, password)
        harvestRegister.pageDataIsLoaded(locationId)
        harvestRegister.sendDeleteReportRequestForTheCurrentDate(locationId)
@@ -61,10 +69,9 @@ describe('Harvest register',function (){
         harvestRegister.reportExistsInTheList(reportDate).next()
             .should('be.visible')
             .and("contain", reportDate)
+    })
 
-})
-
-    it.only('should verify added data in the report',function (){
+    it('should verify added data in the report',function (){
         harvestRegister.addHarvestReport(penM1
             ,fishHarvestedValueForPenM1
             ,averageHarvestWeightValueForM1
@@ -77,8 +84,85 @@ describe('Harvest register',function (){
             ,penM2Comment
             ,successfulToasterPopupMessage)
 
+        harvestRegister.getPenObjectByName(penM1)
+            .should('contain.text', penM1)
+        harvestRegister.addFishHarvestedValue(penM1)
+            .should('have.value', fishHarvestedValueForPenM1)
+        harvestRegister.addAverageHarvestWeightValue(penM1)
+            .should('have.value', averageHarvestWeightValueForM1)
+        harvestRegister.addFishPenAfterHarvestValue(penM1)
+            .should('have.value', fishPenAfterHarvestValueForM1)
+        harvestRegister.addCommentForPen(penM1)
+            .should('have.value', penM1Comment)
+        harvestRegister.getPenObjectByName(penM2)
+            .should('contain.text', penM2)
+        harvestRegister.addFishHarvestedValue(penM2)
+            .should('have.value', fishHarvestedValueForPenM2)
+        harvestRegister.addAverageHarvestWeightValue(penM2)
+            .should('have.value', averageHarvestWeightValueForM2)
+        harvestRegister.addFishPenAfterHarvestValue(penM2)
+            .should('have.value', fishPenAfterHarvestValueForM2)
+        harvestRegister.addCommentForPen(penM2)
+            .should('have.value', penM2Comment)
+    })
 
+    it("should update and verify the report with new values", function (){
+        harvestRegister.addHarvestReport(penM1
+            ,fishHarvestedValueForPenM1
+            ,averageHarvestWeightValueForM1
+            ,fishPenAfterHarvestValueForM1
+            ,penM1Comment
+            ,penM2
+            ,fishHarvestedValueForPenM2
+            ,averageHarvestWeightValueForM2
+            ,fishPenAfterHarvestValueForM2
+            ,penM2Comment
+            ,successfulToasterPopupMessage)
 
+        harvestRegister.addFishHarvestedValue(penM1).clear().type(updatedFishHarvestedValueForPenM1)
+        harvestRegister.addAverageHarvestWeightValue(penM1).clear().type(updatedAverageHarvestWeightValueForM1)
+        harvestRegister.addFishPenAfterHarvestValue(penM1).clear().type(updatedFishPenAfterHarvestValueForM1)
+        harvestRegister.addCommentForPen(penM1).clear().type(updatedPenM1Comment)
+        harvestRegister.addFishHarvestedValue(penM2).clear().type(updatedFishHarvestedValueForPenM2)
+        harvestRegister.addAverageHarvestWeightValue(penM2).clear().type(updatedAverageHarvestWeightValueForM2)
+        harvestRegister.addFishPenAfterHarvestValue(penM2).clear().type(updatedFishPenAfterHarvestValueForM2)
+        harvestRegister.addCommentForPen(penM2).clear().type(updatedPenM2Comment)
+        harvestRegister.saveButton().click()
+
+        harvestRegister.getPenObjectByName(penM1)
+            .should('contain.text', penM1)
+        harvestRegister.addFishHarvestedValue(penM1)
+            .should('have.value', updatedFishHarvestedValueForPenM1)
+        harvestRegister.addAverageHarvestWeightValue(penM1)
+            .should('have.value', updatedAverageHarvestWeightValueForM1)
+        harvestRegister.addFishPenAfterHarvestValue(penM1)
+            .should('have.value', updatedFishPenAfterHarvestValueForM1)
+        harvestRegister.addCommentForPen(penM1)
+            .should('have.value', updatedPenM1Comment)
+        harvestRegister.getPenObjectByName(penM2)
+            .should('contain.text', penM2)
+        harvestRegister.addFishHarvestedValue(penM2)
+            .should('have.value', updatedFishHarvestedValueForPenM2)
+        harvestRegister.addAverageHarvestWeightValue(penM2)
+            .should('have.value', updatedAverageHarvestWeightValueForM2)
+        harvestRegister.addFishPenAfterHarvestValue(penM2)
+            .should('have.value', updatedFishPenAfterHarvestValueForM2)
+        harvestRegister.addCommentForPen(penM2)
+            .should('have.value', updatedPenM2Comment)
+    })
+
+    it.only('should delete the report', function (){
+        harvestRegister.addHarvestReport(penM1
+            ,fishHarvestedValueForPenM1
+            ,averageHarvestWeightValueForM1
+            ,fishPenAfterHarvestValueForM1
+            ,penM1Comment
+            ,penM2
+            ,fishHarvestedValueForPenM2
+            ,averageHarvestWeightValueForM2
+            ,fishPenAfterHarvestValueForM2
+            ,penM2Comment
+            ,successfulToasterPopupMessage)
 
     })
 
--- cypress/support/pageobjects/login-page.js
+++ cypress/support/pageobjects/login-page.js
@@ -5,12 +5,15 @@ export class LoginPage {
 
     userName(){
        return cy.get('#Username');
+       //return cy.get('#1-email') //Auth0 login
     }
     password(){
        return cy.get('#Password');
+       //return cy.get('.auth0-lock-input-password') //Auth0 login
     }
     loginButton(){
         return cy.get('#login-button');
+        //return cy.get('[name="submit"]') //Auth0 login
     }
     validationMessage(){
         this.loadingSpinner()
