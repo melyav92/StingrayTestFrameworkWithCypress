8df7e12459f34ce514a96ccec06da4a959f31ea5
vmelnyk <melyav92@gmail.com>
2022-03-21 19:16:31 +0200

    - Refactored counted box data checking - Added assertion for count of locked in sequences - Added tests that verify sequence list and location history pages data loading

--- cypress/support/pageobjects/LockIn/lock-in-pages.js
+++ cypress/support/pageobjects/LockIn/lock-in-pages.js
@@ -5,6 +5,8 @@ export class LockInPages{
 
     countedBox(){
         return cy.get('.scp-value-box-text-container').first()
+            .invoke('text')
+            .then(parseFloat)
     };
 
     adultFemalesBox(){
@@ -19,13 +21,52 @@ export class LockInPages{
         return cy.get('.scp-day-count-cell-selected')
     };
 
+    countOfLockedInSequencesForDay(){
+        return this.dayIncludedInLockIn().first()
+            .find('button[direction="right"]')
+            .invoke('text')
+            .then(parseFloat)
+    };
+
     threeDotsMenuItemInDayCell(){
         return this.dayIncludedInLockIn().first().find('#dropdown-split-basic')
     };
 
+    viewSequenceListMenuItem(){
+        this.fishCountInput().should('be.visible')
+        return cy.get('.dropdown-item').contains('View sequence-list')
+    };
+
     fishCountInput(){
-        return cy.get('.scp-fish-count-input-container').first().children('input.scp-fish-count-input')
-    }
+        return cy.get('.scp-fish-count-input-container').first()
+            .children('input.scp-fish-count-input')
+    };
+
+    sequenceListTable(){
+        return cy.get('#scp-lock-in-sequence-list-table')
+    };
+
+    pageTitle(){
+      return cy.get('.scp-header-label').last()
+    };
+
+    lockInHistoryButton(){
+        return cy.get('#scp-btn-go-to-lock-in-history')
+    };
+
+    lockInHistoryTable(){
+        return cy.get('#scp-lock-in-history-table')
+    };
+
+    lockedInWeekExistsInTheHistoryTable(){
+        cy.intercept({ method:'GET',
+            pathName: '/weekly-lice-count'}).as('weeklyLiceCount')
+
+        cy.wait('@weeklyLiceCount').its('response.statusCode').should('eq', 200 );
+        this.lockInHistoryTable().should('be.visible');
+        return this.lockInHistoryTable().find('tbody tr')
+    };
+
 
 
 
