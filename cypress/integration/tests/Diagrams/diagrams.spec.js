2628d181e8cd134c8f72c7005e5ae022a4f0c4fb
vmelnyk <melyav92@gmail.com>
2022-03-24 14:33:18 +0200

    - Added tests that verifies manual lice count metrics data is returned without errors - Added test that verifies operations section metrics data is returned without errors - Added test that verifies biometrics section metrics data is returned without errors - Added test that verifies automated diagnostics section metrics data is returned without errors

--- cypress/integration/tests/Diagrams/diagrams.spec.js
+++ cypress/integration/tests/Diagrams/diagrams.spec.js
@@ -15,7 +15,30 @@ describe("Diagrams",function (){
 
     });
 
-    it('should verify that Image based lice count metrics data is returned without errors', function () {
+    it('should verify that Manual lice count metrics: MAN Adult females, MAN Mobiles, MAN Fixed Stages and MAN Calligus data is returned without errors', function () {
+        diagrams.sendSaveManualLiceCountReportRequest();
+        cy.visit(`/en/Results/Diagrams?CustomerId=48&From=2022-03-01&To=2022-03-23&Ms.M[0]=ManualAdultFemaleAverage&Ms.M[1]=ManualMobilesAverage&Ms.M[2]=ManualFixedAverage&Ms.M[3]=ManualCalligusAverage&Dss.L[0]=${Cypress.env('locationId')}`)
+        diagrams.generateButton().click();
+
+        diagrams.manAFLiceMetricInTheLegend()
+            .should('exist')
+            .and('be.visible');
+
+        diagrams.manMobilesMetricInTheLegend()
+            .should('exist')
+            .and('be.visible');
+
+        diagrams.manFixedStagesMetricInTheLegend()
+            .should('exist')
+            .and('be.visible');
+
+        diagrams.manCaligusMetricInTheLegend()
+            .should('exist')
+            .and('be.visible');
+
+    });
+
+    it('should verify that Image based lice count: Adult females and Mobiles metrics data is returned without errors', function () {
         approveObservationsForAllContexts.approveObservationsForAllContexts()
         cy.visit(`/en/Results/Diagrams?CustomerId=48&From=2022-03-01&To=2022-03-23&Ms.M[0]=ImageBasedAdultFemalesAverage&Ms.M[1]=ImageBasedMobilesAverage&Dss.L[0]=${Cypress.env('locationId')}`)
         diagrams.generateButton().click();
@@ -30,7 +53,7 @@ describe("Diagrams",function (){
 
     });
 
-    it.only('should verify that Automated lice count metric data is returned without errors', function () {
+    it('should verify that Automated lice: Adult females count metric data is returned without errors', function () {
         cy.visit(`/en/Results/Diagrams?CustomerId=48&From=2022-03-01&To=2022-03-23&Ms.M[0]=LiceAutomatedCountsAverage&Dss.L[0]=${Cypress.env('locationId')}`)
         diagrams.generateButton().click();
 
@@ -40,7 +63,54 @@ describe("Diagrams",function (){
 
     });
 
-    it('should verify that Image based Diagnostics counts metrics data is returned without errors', function () {
+    it('should verify that pulses, uptime and fish passings metrics data is returned without errors', function () {
+        cy.visit(`/en/Results/Diagrams?CustomerId=48&From=2022-03-01&To=2022-03-23&Ms.M[0]=PulsesSum&Ms.M[1]=UpTimeAverage&Ms.M[2]=FishPassingsSum&Dss.L[0]=${Cypress.env('locationId')}`)
+        diagrams.generateButton().click();
+
+        diagrams.pulsesMetricInTheLegend()
+            .should('exist')
+            .and('be.visible');
+
+        diagrams.upTimeMetricInTheLegend()
+            .should('exist')
+            .and('be.visible');
+
+        diagrams.fishPassingsMetricInTheLegend()
+            .should('exist')
+            .and('be.visible');
+
+    });
+
+    it('should verify that Customer biometrics: MAN Fish stock, MAN Fish weight and MAN biomass metrics data is returned without errors', function () {
+        diagrams.sendSaveBiomassReportRequest()
+        cy.visit(`/en/Results/Diagrams?CustomerId=48&From=2022-03-01&To=2022-03-23&Ms.M[0]=FishStockSum&Ms.M[1]=FishWeightAverage&Ms.M[2]=BiomassSum&Dss.L[0]=${Cypress.env('locationId')}`)
+        diagrams.generateButton().click();
+
+        diagrams.manFishStockMetricInTheLegend()
+            .should('exist')
+            .and('be.visible');
+
+        diagrams.manFishWeightMetricInTheLegend()
+            .should('exist')
+            .and('be.visible');
+
+        diagrams.manBiomassMetricInTheLegend()
+            .should('exist')
+            .and('be.visible');
+
+    });
+
+    it('should verify that automated Stingray biometrics:AUT Fish weight metrics data is returned without errors', function () {
+        cy.visit(`/en/Results/Diagrams?CustomerId=48&From=2022-03-01&To=2022-03-23&Ms.M[0]=StingrayFishWeightAverage&Dss.L[0]=${Cypress.env('locationId')}`)
+        diagrams.generateButton().click();
+
+        diagrams.autFishWeightMetricInTheLegend()
+            .should('exist')
+            .and('be.visible')
+
+    });
+
+    it('should verify that Image based Diagnostics: Fish with ulcers, Mature fish and Snout damage counts metrics data is returned without errors', function () {
         approveObservationsForAllContexts.approveObservationsForAllContexts()
         cy.visit(`/en/Results/Diagrams?CustomerId=48&From=2022-03-01&To=2022-03-23&Ms.M[0]=ImageBasedUlcersAverage&Ms.M[1]=ImageBasedMaturationAverage&Ms.M[2]=ImageBasedSnoutDamageAverage&Dss.L[0]=${Cypress.env('locationId')}`)
         diagrams.generateButton().click();
@@ -58,6 +128,25 @@ describe("Diagrams",function (){
             .and('be.visible');
         
     });
+
+    it('should verify that automated Diagnostics counts metrics data is returned without errors', function () {
+        cy.visit(`/en/Results/Diagrams?CustomerId=48&From=2022-03-01&To=2022-03-23&Ms.M[0]=UlcersAverage&Ms.M[1]=SwimmingSpeedAverage&Ms.M[2]=MaturationAverage&Dss.L[0]=${Cypress.env('locationId')}`)
+        diagrams.generateButton().click();
+
+        diagrams.autFishWithUlcersMetricInTheLegend()
+            .should('exist')
+            .and('be.visible');
+
+        diagrams.autSwimSpeedMetricInTheLegend()
+            .should('exist')
+            .and('be.visible');
+
+        diagrams.autMatureFishMetricInTheLegend()
+            .should('exist')
+            .and('be.visible');
+        
+    });
+
 
 
 })
